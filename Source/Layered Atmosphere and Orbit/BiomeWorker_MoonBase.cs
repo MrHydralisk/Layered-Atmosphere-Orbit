4f2e67540561050b33bfea06e4b4d283f43cad4e
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-09-19 01:55:45 +0300

    Added Luna biome. Renamed basic Moon to Luna. Added description to Luna layer.

---
+++ Source/Layered Atmosphere and Orbit/BiomeWorker_MoonBase.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections;
+using UnityEngine;
+using Verse;
+
+namespace LayeredAtmosphereOrbit
+{
+    public class BiomeWorker_MoonBase : BiomeWorker
+    {
+        public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+        {
+            return 0;
+        }
+    }
+}
+
